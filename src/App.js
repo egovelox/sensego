35475f4c4882af4e44812c5d30c8f89aed1ab2b1
egovelox <egovelox@protonmail.com>
2020-02-20 01:30:40 +0100

    added key on <Resort /> loop

--- src/App.js
+++ src/App.js
@@ -43,9 +43,10 @@ class App extends Component {
       <main className="my-5 py-5">
         <Container className="px-0">
         <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
-        {resorts.slice(0,30).map(resort => (
+        {resorts.slice(0,30).map((resort, index) => (
           <Col xs={{ order: 2 }} md={{ size: 3, order: 1 }} tag="aside" className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
-          <Resort
+          <Resort 
+            key={index}
             name={resort.Offer_Name}
             image={resort.Image}
             text={resort.Text}
